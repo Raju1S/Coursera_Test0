9b0b31d0fd88280d79924c34040ec1154a5d2e17
unknown <rakshant8@gmail.com>
2021-07-23 23:41:42 +0530

    Module Four

--- Assignments/Module4/SpeakHello.js
+++ Assignments/Module4/SpeakHello.js
@@ -18,14 +18,12 @@
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
-
-(function (window){
+(function (window) {
   var speakWord = "Hello";
-  
   var helloSpeaker = function (name) {
     console.log(speakWord + " " + name);
   }
-  
+
   window.helloSpeaker = helloSpeaker;
-  
+
 })(window);
